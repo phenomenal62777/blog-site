030e1b4c0d0fb81bf9999986f74e490a4248f722
phenomenal62777 <58382760+phenomenal62777@users.noreply.github.com>
2022-01-29 21:55:49 +0500

    Add files via upload
    download zip file and then npm install and then run server

---
+++ firebase.js
@@ -0,0 +1,11 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyBRSS2AaMDr68_iRDgAEsUITuHVFwInDwk",
+    authDomain: "blogsite-264cc.firebaseapp.com",
+    projectId: "blogsite-264cc",
+    storageBucket: "blogsite-264cc.appspot.com",
+    messagingSenderId: "79574059970",
+    appId: "1:79574059970:web:5d6bcab29c083fb655645b"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
